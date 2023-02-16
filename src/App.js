1231c668e68bc776b24d6d750b5a1bffa60cbd3a
smita-raj12 <smita.raj12@gmail.com>
2023-02-15 20:27:49 -0800

    Display 3 city whether forecast

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
 
 import './App.css';
-
+import TexasForecast from './TexasForecast';
+import RenoForecast from './RenoForecast';
+import TampaForecast from './TampaForecast';
 function App() {
   return (
-    <div className="App">
-      My app
-    </div>
-  );
+    <>
+    <TexasForecast />
+    <RenoForecast />
+    <TampaForecast />
+    </>
+  )
 }
 
 export default App;
---
+++ src/TexasForecast.js
@@ -0,0 +1,39 @@
+/* eslint-disable jsx-a11y/alt-text */
+
+import { useState, useEffect } from 'react';
+
+export default function TexasForecast() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+  
+            useEffect(() => {
+            fetch('http://api.weatherapi.com/v1/current.json?key=e44d5f80bc8e47009f5225012231502&q=Austin Tx')
+            .then(res => res.json())
+            .then(
+              (jsonifiedResponse) => {
+                console.log(jsonifiedResponse)
+                setIsLoaded(true);
+                setItems(jsonifiedResponse);
+              },
+              (error) => {
+                setIsLoaded(true);
+                setError(error);
+              }
+            )
+          }, [])
+       
+          if (error) {
+            return <div>Error: {error.message}</div>;
+          } else if (!isLoaded) {
+            return <div>Loading...</div>;
+          } else {
+            return (
+              <ul>
+                {items.location.name} <br />
+                Max Temp{items.current.temp_f}
+                <img src={items.current.condition.icon} />
+              </ul>
+            );
+          }
+}
